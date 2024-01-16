48665ad988d400b469d94a498ac765036279f9da
jayesh-webplanex <jayeshs@webplanex.com>
2024-01-16 16:36:07 +0530

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,74 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'holderjs'
+import NavBar from './components/navbar';
+import Footer from './components/footer';
+
 function App() {
+
   return (
+    
     <div className="App">
+
+        <NavBar />
+       
+       <Container className='py-5'>
+      <Row>
+        <Col>
+        <Card style={{ width: '20rem' }}>
+          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+        </Col>
+        <Col>
+        <Card style={{ width: '20rem' }}>
+          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+        </Col>
+        <Col>
+        <Card style={{ width: '20rem' }}>
+          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+        </Col>
+      </Row>
+    </Container>
+
+    <Footer />
+   
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edited <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -18,6 +79,7 @@ function App() {
           Learn React
         </a>
       </header>
+    
     </div>
   );
 }
